00679435e048b6a01cc0964b6f4334d1ebe8b80b
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-08-07 10:22:40 +0800

    gets ko na

--- public/script.js
+++ public/script.js
@@ -1,5 +1,4 @@
-let age;
-age = 13;
+let age = 13;
 
 let maxAge;
 maxAge = 100; 
@@ -9,3 +8,6 @@ numberperday = 2;
 
 let totalRequired
 totalRequired = (numberperday*365)*(maxAge - age)
+
+let result = "You will need" + totalRequired + "packs of chips to last you until the ripe old age of" + maxAge + ".";
+document.write(result)
