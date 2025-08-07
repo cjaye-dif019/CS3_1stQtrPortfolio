fcfeae5a2e5fc0f36e334ac26764a61102adba87
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-08-07 10:24:41 +0800

    1st Qtr. Non Graded

--- public/script.js
+++ public/script.js
@@ -9,5 +9,5 @@ numberperday = 2;
 let totalRequired
 totalRequired = (numberperday*365)*(maxAge - age)
 
-let result = "You will need" + totalRequired + "packs of chips to last you until the ripe old age of" + maxAge + ".";
+let result = "You will need " + totalRequired + " packs of chips to last you until the ripe old age of " + maxAge + ".";
 document.write(result)
