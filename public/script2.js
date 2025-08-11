dc699dfd0856bbabeaa239995272bde12ecfaa91
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-08-11 20:11:34 +0800

    debugging

--- public/script2.js
+++ public/script2.js
@@ -1,2 +1,17 @@
 //NG3
 
+    let sure = window.confirm("Sure of your numbers?")
+    if (sure) 
+    {
+        let totalRequired = (Number(numberperday) * 365) * (Number(maxAge) - Number(age));
+
+        document.write("You will need " + totalRequired + " to last until the ripe old age of " + maxAge + ".")
+
+        alert(`You will need ${totalRequired} to last you until the ripe old age of ${maxAge} .`);
+
+        console.log("You will need "+ totalRequired + " to last you until the ripe old age of " + maxAge + ".");
+    }
+    else
+    {
+        window.alert("Please click run again.")
+    }
