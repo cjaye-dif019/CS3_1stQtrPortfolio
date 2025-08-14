ae7d6c1f62b9ea38f6d3551d1043418600b767d6
cjaye-dif019 <b2029cjadifuntorum@pshs.edu.ph>
2025-08-14 10:39:34 +0800

    SPEED TEST

--- public/script2.js
+++ public/script2.js
@@ -1,2 +1,12 @@
 //NG3
 
+function result() 
+{
+    let age = Number(document.getElementById("age").value);
+    let maxAge  = Number(document.getElementById("maxAge").value);
+    let NumPerday = Number(document.getElementById("numPerday").value);
+
+    let totalRequired = (Number(NumPerday) * 365) * (Number(maxAge) - Number(age));
+
+    document.getElementById("resultHere").innerHTML = `<b> You will need ${totalRequired} to last you until the ripe old age of ${maxAge}.</b>`;
+}
